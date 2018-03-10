6706c607a85f1682aba4f4cb486e7f70320b8a0f
Grizzly232 <michael.brady.ie@gmail.com>
2018-03-10 15:07:39 +0000

    five features implemented

--- play-java-seed/app/controllers/HomeController.java
+++ play-java-seed/app/controllers/HomeController.java
@@ -1,14 +1,14 @@
 package controllers;
 
-import io.ebean.Ebean;
-import io.ebean.ExpressionList;
-import io.ebean.SqlQuery;
+import io.ebean.*;
 import play.mvc.*;
 
 import models.*;
 import views.html.*;
 import models.database.DatabaseOperator;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 /**
@@ -17,15 +17,15 @@ import java.util.*;
  */
 public class HomeController extends Controller {
 
-	Random rand = new Random();
+    Random rand = new Random();
     DatabaseOperator db = new DatabaseOperator();
 
     List<Project> projList;
-	 
+
     public Result index() {
         db.fillEmployee(5);
         List<Employee> empList = Employee.findAll();
-        return ok( index.render("hi"));
+        return ok(index.render("hi"));
     }
 
     public Result projects() {
@@ -34,23 +34,16 @@ public class HomeController extends Controller {
         projList = Project.findAll();
 
         db.log.debug("this is a fucken debug message");
-		return ok(projects.render("wilkommen", projList));
-	}
+        return ok(projects.render("wilkommen", projList));
+    }
 
-	public Result project(String name) {
+    public Result project(String name) {
         projList = Project.findAll();
         Project projToLoad = new Project();
         List<Employee> collaborators = projToLoad.getCollaborators();
-        SqlQuery sql = Ebean.createSqlQuery("SELECT * FROM PROJECT_EMPLOYEE");
-
-        for(Project p : projList) {
-            if(p.getProjectName().equals(name))
-                projToLoad = p;
-        }
-
-        Employee emp = Ebean.find(Employee.class, 3);
-        db.log.debug("Employee: " + emp.getEmp_id());
-        db.addToAssociation(projToLoad, emp);
+        collaborators.add(Ebean.find(Employee.class, 3));
+        collaborators.add(Ebean.find(Employee.class, 4));
+        collaborators.add(Ebean.find(Employee.class, 1));
 
         return ok(project.render(name, projToLoad, collaborators));
     }
--- play-java-seed/target/scala-2.12/twirl/main/views/html/main.template.scala
+++ play-java-seed/target/scala-2.12/twirl/main/views/html/main.template.scala
@@ -22,15 +22,15 @@ import play.mvc.Http.Context.Implicit._
 import play.data._
 import play.core.j.PlayFormsMagicForJava._
 
-object main extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template3[String,Html,Html,play.twirl.api.HtmlFormat.Appendable] {
+object main extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template2[String,Html,play.twirl.api.HtmlFormat.Appendable] {
 
   /**/
-  def apply/*1.2*/(title: String, scripts: Html = Html(""))(content: Html):play.twirl.api.HtmlFormat.Appendable = {
+  def apply/*1.2*/(title: String)(content: Html):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 
 
-Seq[Any](format.raw/*1.58*/("""
+Seq[Any](format.raw/*1.32*/("""
 
 """),format.raw/*3.1*/("""<!DOCTYPE html>
 <html lang="en">
@@ -40,8 +40,7 @@ Seq[Any](format.raw/*1.58*/("""
         <link rel="stylesheet" media="all" href=""""),_display_(/*8.51*/routes/*8.57*/.Assets.versioned("stylesheets/bootstrap.min.css")),format.raw/*8.107*/("""">
         <link rel="stylesheet" media="all" href=""""),_display_(/*9.51*/routes/*9.57*/.Assets.versioned("stylesheets/main.css")),format.raw/*9.98*/("""">
         <link rel="shortcut icon" type="image/png" href=""""),_display_(/*10.59*/routes/*10.65*/.Assets.versioned("images/favicon.png")),format.raw/*10.104*/("""">
-        """),_display_(/*11.10*/scripts),format.raw/*11.17*/("""
-    """),format.raw/*12.5*/("""</head>
+    </head>
     <body>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -52,8 +51,8 @@ Seq[Any](format.raw/*1.58*/("""
         </div>
         <div class="container-fluid">
             <ul class="nav navbar-nav buttons">
-                <li><a href=""""),_display_(/*23.31*/routes/*23.37*/.HomeController.index()),format.raw/*23.60*/("""">Home</a></li>
-                <li><a href=""""),_display_(/*24.31*/routes/*24.37*/.HomeController.projects()),format.raw/*24.63*/("""">Projects</a></li>
+                <li><a href=""""),_display_(/*22.31*/routes/*22.37*/.HomeController.index()),format.raw/*22.60*/("""">Home</a></li>
+                <li><a href=""""),_display_(/*23.31*/routes/*23.37*/.HomeController.projects()),format.raw/*23.63*/("""">Projects</a></li>
                 <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Log in
                     <span class="caret"></span></a>
                     <ul class="dropdown-menu">
@@ -67,18 +66,18 @@ Seq[Any](format.raw/*1.58*/("""
                 </li>
             </ul>
         </div>
-        """),format.raw/*39.31*/("""
-        """),_display_(/*40.10*/content),format.raw/*40.17*/("""
-        """),format.raw/*41.9*/("""<div class="container-fluid footer">
+        """),format.raw/*38.31*/("""
+        """),_display_(/*39.10*/content),format.raw/*39.17*/("""
+        """),format.raw/*40.9*/("""<div class="container-fluid footer">
             <h4>JCMB Inc. &copy 2018</h4>
             <a href="#">Back to top</a>
         </div>
 
-        <script src=""""),_display_(/*46.23*/routes/*46.29*/.Assets.versioned("javascripts/jquery-3.2.1.slim.js")),format.raw/*46.82*/(""""></script>
-        <script src=""""),_display_(/*47.23*/routes/*47.29*/.Assets.versioned("javascripts/main.js")),format.raw/*47.69*/(""""></script>
-        <script src=""""),_display_(/*48.23*/routes/*48.29*/.Assets.versioned("javascripts/bootstrap.js")),format.raw/*48.74*/(""""></script>
-        <script src=""""),_display_(/*49.23*/routes/*49.29*/.Assets.versioned("javascripts/bootstrap.min.js")),format.raw/*49.78*/(""""></script>
-        <script src=""""),_display_(/*50.23*/routes/*50.29*/.Assets.versioned("javascripts/main.js")),format.raw/*50.69*/(""""></script>
+        <script src=""""),_display_(/*45.23*/routes/*45.29*/.Assets.versioned("javascripts/jquery-3.2.1.slim.js")),format.raw/*45.82*/(""""></script>
+        <script src=""""),_display_(/*46.23*/routes/*46.29*/.Assets.versioned("javascripts/main.js")),format.raw/*46.69*/(""""></script>
+        <script src=""""),_display_(/*47.23*/routes/*47.29*/.Assets.versioned("javascripts/bootstrap.js")),format.raw/*47.74*/(""""></script>
+        <script src=""""),_display_(/*48.23*/routes/*48.29*/.Assets.versioned("javascripts/bootstrap.min.js")),format.raw/*48.78*/(""""></script>
+        <script src=""""),_display_(/*49.23*/routes/*49.29*/.Assets.versioned("javascripts/main.js")),format.raw/*49.69*/(""""></script>
 
     </body>
 </html>
@@ -87,9 +86,9 @@ Seq[Any](format.raw/*1.58*/("""
     }
   }
 
-  def render(title:String,scripts:Html,content:Html): play.twirl.api.HtmlFormat.Appendable = apply(title,scripts)(content)
+  def render(title:String,content:Html): play.twirl.api.HtmlFormat.Appendable = apply(title)(content)
 
-  def f:((String,Html) => (Html) => play.twirl.api.HtmlFormat.Appendable) = (title,scripts) => (content) => apply(title,scripts)(content)
+  def f:((String) => (Html) => play.twirl.api.HtmlFormat.Appendable) = (title) => (content) => apply(title)(content)
 
   def ref: this.type = this
 
@@ -98,11 +97,11 @@ Seq[Any](format.raw/*1.58*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Tue Mar 06 17:25:52 GMT 2018
+                  DATE: Sat Mar 10 14:46:32 GMT 2018
                   SOURCE: C:/Users/micha/Documents/GitHub/SDevCA1/play-java-seed/app/views/main.scala.html
-                  HASH: cfa92c0aa61f0d7a91dd3b2da3fec99c46b1a6f5
-                  MATRIX: 957->1|1108->57|1136->59|1222->119|1247->124|1332->183|1346->189|1413->235|1492->288|1506->294|1577->344|1656->397|1670->403|1731->444|1819->505|1834->511|1895->550|1934->562|1962->569|1994->574|2445->998|2460->1004|2504->1027|2577->1073|2592->1079|2639->1105|3457->1984|3494->1994|3522->2001|3558->2010|3742->2167|3757->2173|3831->2226|3892->2260|3907->2266|3968->2306|4029->2340|4044->2346|4110->2391|4171->2425|4186->2431|4256->2480|4317->2514|4332->2520|4393->2560
-                  LINES: 28->1|33->1|35->3|38->6|38->6|39->7|39->7|39->7|40->8|40->8|40->8|41->9|41->9|41->9|42->10|42->10|42->10|43->11|43->11|44->12|55->23|55->23|55->23|56->24|56->24|56->24|70->39|71->40|71->40|72->41|77->46|77->46|77->46|78->47|78->47|78->47|79->48|79->48|79->48|80->49|80->49|80->49|81->50|81->50|81->50
+                  HASH: ae6b67247856679202aea8850c0ac695aa32b038
+                  MATRIX: 952->1|1077->31|1105->33|1191->93|1216->98|1301->157|1315->163|1382->209|1461->262|1475->268|1546->318|1625->371|1639->377|1700->418|1788->479|1803->485|1864->524|2322->955|2337->961|2381->984|2454->1030|2469->1036|2516->1062|3334->1941|3371->1951|3399->1958|3435->1967|3619->2124|3634->2130|3708->2183|3769->2217|3784->2223|3845->2263|3906->2297|3921->2303|3987->2348|4048->2382|4063->2388|4133->2437|4194->2471|4209->2477|4270->2517
+                  LINES: 28->1|33->1|35->3|38->6|38->6|39->7|39->7|39->7|40->8|40->8|40->8|41->9|41->9|41->9|42->10|42->10|42->10|54->22|54->22|54->22|55->23|55->23|55->23|69->38|70->39|70->39|71->40|76->45|76->45|76->45|77->46|77->46|77->46|78->47|78->47|78->47|79->48|79->48|79->48|80->49|80->49|80->49
                   -- GENERATED --
               */
           
